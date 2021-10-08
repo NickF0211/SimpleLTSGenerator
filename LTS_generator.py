3b297b143b32ffb5c787af2c6e33458944914011
NickF <lkbjhgfd@hotmail.com>
2021-10-08 18:20:21 -0400

    sync

--- LTS_generator.py
+++ LTS_generator.py
@@ -1,5 +1,6 @@
 from actionPool import *
 from graph import *
+import os
 
 def generate_single_LTS(target_name, b_action, b_var, b_time, b_shared, b_depth, b_shared_ratio = -1):
     ap = ActionPool(b_action, b_var, b_time, b_shared, b_shared_ratio= b_shared_ratio)
@@ -9,5 +10,15 @@ def generate_single_LTS(target_name, b_action, b_var, b_time, b_shared, b_depth,
     with open(target_name, 'w') as file:
         file.write(graph.write_LTS())
 
-def generate_LTS(target_directory, b_action, b_var, b_time, b_shared, b_depth, b_shared_ratio = -1):
-    return
+def generate_LTS(target_directory, N, b_action, b_var, b_time, b_shared, b_depth, b_shared_ratio = -1):
+    if not os.path.isdir(target_directory):
+        os.mkdir(target_directory)
+    ap = ActionPool(b_action, b_var, b_time, b_shared, b_shared_ratio= b_shared_ratio)
+    for i in range(N):
+        target_file = os.path.join(os.getcwd(), target_directory, "graph_{}.aut".format(i))
+        graph = Graph(b_depth, ap.ap_size)
+        graph.generate()
+        ap.label(graph)
+        with open(target_file, 'w') as file:
+            file.write(graph.write_LTS())
+
--- test_graph.py
+++ test_graph.py
@@ -1,10 +1,15 @@
-from LTS_generator import generate_single_LTS
+from LTS_generator import generate_LTS
 import os.path
 if __name__ == "__main__":
     #file name, #num of action, #argument bound, #time progression bound, #shared action bound #LTS math depth, #shared ratio
     result_dir = "LTS_folder"
     N = 5
+    '''
     for i in range(N):
         generate_single_LTS("grah_{}.aut".format(i), 2, [2, 2], 2, 1, 2, b_shared_ratio=0.1)
+    '''
+    #generate_LTS(target_directory, N, b_action, b_var, b_time, b_shared, b_depth, b_shared_ratio = -1):
+    generate_LTS(result_dir, 5, 5, [100,100], 1, 5, 10, b_shared_ratio = 0.1)
+
 
 
